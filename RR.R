5980999c545fa8ce2af6e7cc7997de987df1f10a
Saturn <saturn.jian.chen@gmail.com>
2017-01-10 16:21:45 +1100

    Stage 1 - get things basically working

--- RR.R
+++ RR.R
@@ -1,55 +1,55 @@
 # Reference Relationship Tool
+# Jian Chen
+# 09/01/2017
 
 setwd("/Users/JIANCHEN/GitHub/Reference_Relationship")
 
-## pdf processing toolbox
-install.packages("pdftools")
-require(pdftools)
-
-txt <- pdf_text("1403.2805.pdf")
-txt
-
-info <- pdf_info("1403.2805.pdf")
-info
-
-fontspdf <- pdf_fonts("1403.2805.pdf")
-
-attachments <- pdf_attachments("1403.2805.pdf")
-attachments
-
-toc <- pdf_toc("1403.2805.pdf")
-a <- jsonlite::toJSON(toc, auto_unbox = TRUE, pretty = TRUE)
-a
-
-
-
-
-
-## metadata request
-install.packages("dplyr") # The devtools package provides install_github() that enables installing packages from GitHub.
-library(devtools)
-install_github('rmetadata', 'ropensci') # uncomment to install
-library(rmetadata)
-# For DataCite.
-count_identifiers("datacite")
-print(crossref_citation("10.3998/3336451.0009.101"), style = "Bibtex")
-
-
 ## install pdf extract tool in your macOS (my version is 10.12.2) via Terminal
-#  # https://github.com/CrossRef/pdfextract
-# use ruby in terminal, can extract referece part
-# Open Terminal
+#  use ruby in terminal, can extract referece part
+# https://github.com/CrossRef/pdfextract
+# 
+# Open Terminal and input command: 
 #   sudo gem pdf-extract
-#   sudo gem uninstall pdf-reader              # remove old version 
+#   sudo gem uninstall pdf-reader              # remove current version 
 #   sudo gem install pdf-reader -v 1.2.0       # install version 1.2.0
 
 
+# set pdf file path
+pdfpath <- c("/Users/JIANCHEN/GitHub/Reference_Relationship/pdfs/")
 
-## use system command in r
-# get reference part of a pdf document
-ref = system("pdf-extract extract --references /Users/JIANCHEN/GitHub/Reference_Relationship/pdfs/1403.2805.pdf", intern=TRUE)
-ref[22]
-ref = system("pdf-extract extract --references /Users/JIANCHEN/GitHub/Reference_Relationship/pdfs/4.pdf", intern=TRUE)
-ref[30]
+# remove spaces in folder names
+pathname <- list.dirs(path = pdfpath, full.names = TRUE, recursive = TRUE)
+pathname_new <- stri_replace_all_fixed(pathname, " ", "_")
+file.rename(from = pathname, to = pathname_new)
+
+# find all pdf filenames in the given folder
+pdfname <- list.files(pdfpath,recursive = TRUE) # list all filenames (including subfolders (no space in folder name))
+
+# remove spaces in filenames
+library(stringi)
+pdfname_new <- stri_replace_all_fixed(pdfname, " ", "_")  # replace space with underscore
+# rename pdf files
+file.rename(from = paste0(pdfpath,pdfname), to = paste0(pdfpath,pdfname_new))
+
+
+## use system command in r to get reference and title of a pdf document
+paper <- list()
+papers <- list()
+fileindex <- character()
+systemcmd_ref <- character()
+systemcmd_tit <- character()
+
+for (i in 1:length(pdfname_new)){
+  
+  fileindex[i] <- paste(pdfpath,pdfname_new[i],sep = "")
+  systemcmd_ref[i] <- paste("pdf-extract extract --references ",fileindex[i])
+  systemcmd_tit[i] <- paste("pdf-extract extract --titles ",fileindex[i])
+  
+  paper$ref <- system(systemcmd_ref[i], intern=TRUE)  # get reference using system command and pdfextract tool
+  paper$title <- system(systemcmd_tit[i], intern=TRUE) # get title using system command and pdfextract tool
+  var_paper <- paste('paper',i,sep = "")
+  papers[[var_paper]] <- paper
+}
+
 
 
